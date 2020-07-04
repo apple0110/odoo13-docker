36df5fc44edc80f1538ed097ef207a0257a7336b
apple <apple@odrapp.com>
2020-07-05 00:47:09 +0600

    Init docker for odoo13

---
+++ enterprise/TeamOdoo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "TeamOdoo",
+    'version': '13.0.1.0.0',
+    'summary': "Bangladeshi Payment Getway",
+    'description': "Details will be here",
+    'category': 'Payment Getway',
+    'sequence': 1,
+    'author': "TeamOdoo",
+    'company': 'TeamOdoo',
+    'maintainer': 'TeamOdoo',
+    'website': "https://www.teamOdoo.com",
+    'depends': ['base', 'mail'],
+    'data': [
+    ],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': True,
+}
